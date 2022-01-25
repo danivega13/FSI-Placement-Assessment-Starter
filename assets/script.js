2d25d2da4fcf395bc63fe80b452896da13211f8a
Daniela Vega <danielavega@MacBook-Pro.local>
2022-01-24 20:38:02 -0500

    updated with final assessment files

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,89 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+// NAME UPDATE
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+let yourName = "Daniela Vega" 
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+// COOKIE VARIABLES
+let gb = 0      // Gingerbread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
+let addGingerbread = document.getElementById('add-gb')
+let minusGingerbread = document.getElementById('minus-gb')
+let minusCC = document.getElementById('minus-cc')
+let addCC =  document.getElementById('add-cc')
+let minusSugar = document.getElementById('minus-sugar')
+let addSugar = document.getElementById('add-sugar')
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+let updateGbCount = 0
+let updateCCcount = 0
+let updateSugarCount = 0
+let total = 0
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// NAME UPDATE
+document.getElementById('credit').textContent = `Created by ${yourName}`
+
+// GINGERBREAD EVENT LISTENERS
+addGingerbread.addEventListener('click', function() {
+   if(addGingerbread){
+    updateGbCount++;
+    total++;
+    document.getElementById('qty-gb').innerHTML = (updateGbCount);
+    document.getElementById('qty-total').innerHTML = (total);
+   }
 })
+ 
+minusGingerbread.addEventListener('click', function(){
 
-// TODO: Hook up event listeners for the rest of the buttons
+    if(updateGbCount > 0){
+        updateGbCount --; 
+        total--;
+        document.getElementById('qty-gb').innerHTML = (updateGbCount);
+        document.getElementById('qty-total').innerHTML = (total);
+    }
+
+
+ })
+
+// CHOCOLATE CHIP EVENT LISTENERS
+
+ addCC.addEventListener('click', function() { 
+   if(addCC){
+    updateCCcount++;
+    total++;
+    document.getElementById('qty-cc').innerHTML = (updateCCcount);
+    document.getElementById('qty-total').innerHTML = (total);
+   }
+})
+ 
+minusCC.addEventListener('click', function(){
+
+    if(updateCCcount > 0){
+
+        updateCCcount --; 
+        total--;
+        document.getElementById('qty-cc').innerHTML = (updateCCcount);
+        document.getElementById('qty-total').innerHTML = (total);
+
+    }
+ })
+
+// SUGAR COOKIE EVENT LISTENERS
+
+addSugar.addEventListener('click', function() {
+    if(addSugar){
+     updateSugarCount++;
+     total++;
+    document.getElementById('qty-sugar').innerHTML = (updateSugarCount);
+    document.getElementById('qty-total').innerHTML = (total);
+    }
+ })
+ 
+ minusSugar.addEventListener('click', function(){
+     if(updateSugarCount > 0){
+ 
+         updateSugarCount --; 
+         total--;
+         document.getElementById('qty-sugar').innerHTML = (updateSugarCount);
+         document.getElementById('qty-total').innerHTML = (total);
+     }
+    })
